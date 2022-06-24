3ecc54a21ff8907f528659d7369ff1195bfded70
unknown <amooruth11@gmail.com>
2022-06-24 03:05:27 +0100

    profile

---
+++ intro.js
@@ -0,0 +1,11 @@
+
+console.log('Name: Amoo Ruth')
+console.log('Height: 5ft')
+console.log('Country: Nigeria')
+
+
+document.writeln('Name: Amoo Ruth')
+document.write('<br>')
+document.writeln('Height: 5ft')
+document.write('<br>')
+document.writeln('Country: Nigeria')
